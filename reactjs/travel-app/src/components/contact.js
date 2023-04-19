25532b202ae74927cdce1da4f92bb7ef66414f99
Pavan S <4mh19ec060@gmail.com>
2023-04-19 20:19:49 +0530

    Add files via upload

---
+++ reactjs/travel-app/src/components/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Phone: 555-1234</p>
+      <p>Email: info@travelsite.com</p>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ reactjs/travel-app/src/components/destination.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Destinations() {
+  return (
+    <div>
+      <h1>Our Destinations</h1>
+      <ul>
+        <li>Paris, France</li>
+        <li>Tokyo, Japan</li>
+        <li>New York, USA</li>
+        <li>Sydney, Australia</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Destinations;
